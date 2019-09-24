39bc8f555cd819e07fd8f640b955fd74fe4d63f1
Jun Luo <luo2jun1@gmail.com>
2019-09-24 14:42:16 -0400

    Turn environment into OO style implementation

--- env.py
+++ env.py
@@ -1,47 +1,150 @@
 from random import random
 
-# TODO: OO
 # TODO: write tests
 # TODO: add YAML configuration
 
-environment_size = environment_size_x, environment_size_y = 0.6, 0.6 # meters
-environment_center = environment_center_x, environment_center_y = 0.0, 0.0
-environment_min_x, environment_min_y = environment_center_x - environment_size_x / 2, environment_center_y - environment_size_y / 2
-background_color = (64, 128, 0, 255) # RGBA
+def test():
+  env = Environment()
 
-board_size = board_size_x, board_size_y = environment_size_x / 3.0 * 2.0, environment_size_y / 3.0 * 2.0 # allow size_x and size_y to be different
-board_center = board_center_x, board_center_y = environment_center_x, environment_center_y # center of board
-board_min_x, board_min_y = board_center_x - board_size_x / 2, board_center_y - board_size_y / 2
-board_color = (128, 64, 32, 255) # RGBA
-board_lines = 4 # 4 for tic-tac-toe; 19 for Go
-board_inset_x = board_size_x / (board_lines + 1)
-board_inset_y = board_size_y / (board_lines + 1)
-board_line_min_x = board_min_x + board_inset_x
-board_line_min_y = board_min_y + board_inset_y
-board_line_max_x = board_min_x + board_size_x - board_inset_x
-board_line_max_y = board_min_y + board_size_y - board_inset_y
-board_line_inc_x = board_inset_x
-board_line_inc_y = board_inset_y
-board_x_line_endpoints = [((board_line_min_x, board_line_min_y + board_line_inc_y * n), (board_line_max_x, board_line_min_y + board_line_inc_y * n)) for n in range(board_lines)]
-board_y_line_endpoints = [((board_line_min_x + board_line_inc_x * n, board_line_min_y), (board_line_min_x + board_line_inc_x * n, board_line_max_y)) for n in range(board_lines)]
-board_line_color = (32, 32, 32, 255) # RGBA
+  board = env.getBoard()
 
-number_of_stones = 10 # 10 for tic-tac-toe; 360 for Go
-stone_size = min(board_inset_x, board_inset_y) / 5 * 4
-stone_radius = stone_size / 2
-stone_color_white = (224, 224, 224, 255)
-stone_color_black = (32, 32, 32, 255)
+  lines = board.getLines()
+  for (start_pos, end_pos) in lines:
+    print(board.getLineColor(), start_pos, end_pos)
 
-# scale [0, 1] to [stone_radius, size_x - stone_radius]
-stones = [((stone_color_white if (n % 2) == 0 else stone_color_black), (environment_min_x + stone_radius + random() * (environment_size_x - stone_size), stone_radius + random() * (environment_min_y + environment_size_y - stone_size)), stone_radius) for n in range(number_of_stones)]
+  stones = env.getStones()
+  for stone in stones:
+    print(stone.getColor(), stone.getCenter(), stone.getRadius())
 
-origin = environment_center # origin is at center of environment
 
-for (start_pos, end_pos) in board_x_line_endpoints:
-    print(board_line_color, start_pos, end_pos)
-for (start_pos, end_pos) in board_y_line_endpoints:
-    print(board_line_color, start_pos, end_pos)
+class Environment:
 
-for (color, center, radius) in stones:
-    print(color, center, radius)
+  def getBackgroundColor(self):
+    return self._background_color
+
+  def getEnvironmentSize(self):
+    return self._environment_size
+
+  def getBounds(self):
+    return self._bounds
+
+  def getBoard(self):
+    return self._board
+
+  def getStones(self):
+    return self._stones
+
+  def __init__(self, size_x=0.6, size_y=0.6, board_lines=4, number_of_stones=10): # 4 & 10 for tic-tac-toe
+    self._background_color = (64, 128, 0, 255) # RGBA
+    self._environment_size = size_x, size_y
+    self._environment_center = center_x, center_y = 0.0, 0.0
+    min_x, min_y = center_x - size_x / 2, center_y - size_y / 2
+    max_x, max_y = center_x + size_x / 2, center_y + size_y / 2
+    self._bounds = (min_x, min_y, max_x, max_y)
+    self._board = Board(self._environment_size, self._environment_center, board_lines)
+
+    board_inset_x, board_inset_y = self._board._inset
+    stone_size = min(board_inset_x, board_inset_y) / 5 * 4
+    self._stones = self._init_stones(number_of_stones, stone_size)
+
+  def _init_stones(self, number_of_stones, stone_size):
+    stone_radius = stone_size / 2
+    stone_color_white = (224, 224, 224, 255)
+    stone_color_black = (32, 32, 32, 255)
+
+    environment_center_x, environment_center_y = self._environment_center
+    environment_size_x, environment_size_y = self._environment_size
+    environment_min_x, environment_min_y, _, _ = self._bounds
+
+    stones = []
+    for n in range(number_of_stones):
+      stone_color = (stone_color_white if (n % 2) == 0 else stone_color_black)
+      stone_center = (
+        environment_min_x + stone_radius + random() * (environment_size_x - stone_size),
+        stone_radius + random() * (environment_min_y + environment_size_y - stone_size)
+      )
+      stone = Stone(stone_color, stone_radius, stone_center)
+      stones.append(stone)
+    return stones
+
+
+class Board:
+
+  def getColor(self):
+    return self._color
+
+  def getSize(self):
+    return self._size
+
+  def getLineColor(self):
+    return self._line_color
+
+  def getNumberOfLines(self):
+    return self._number_of_lines # in one direction
+
+  def getLines(self):
+    return self._lines
+
+  def getInset(self):
+    return self._inset
+
+  def __init__(self, environment_size, environment_center, board_lines):
+    self._color = (128, 64, 32, 255) # RGBA
+    self._line_color = (32, 32, 32, 255) # RGBA
+    environment_size_x, environment_size_y = environment_size
+    environment_center_x, environment_center_y = environment_center
+    self._size = board_size_x, board_size_y = environment_size_x / 3.0 * 2.0, environment_size_y / 3.0 * 2.0 # allow size_x and size_y to be different
+    self._center = board_center_x, board_center_y = environment_center_x, environment_center_y # center of board
+    self._number_of_lines = board_lines
+
+    board_min_x, board_min_y = board_center_x - board_size_x / 2, board_center_y - board_size_y / 2
+    board_lines = 4 # 4 for tic-tac-toe; 19 for Go
+    board_inset_x = board_size_x / (board_lines + 1)
+    board_inset_y = board_size_y / (board_lines + 1)
+    board_line_min_x = board_min_x + board_inset_x
+    board_line_min_y = board_min_y + board_inset_y
+    board_line_max_x = board_min_x + board_size_x - board_inset_x
+    board_line_max_y = board_min_y + board_size_y - board_inset_y
+    board_line_inc_x = board_inset_x
+    board_line_inc_y = board_inset_y
+
+    self._inset = (board_inset_x, board_inset_y)
+
+    x_lines = [((board_line_min_x, board_line_min_y + board_line_inc_y * n), (board_line_max_x, board_line_min_y + board_line_inc_y * n)) for n in range(board_lines)]
+    y_lines = [((board_line_min_x + board_line_inc_x * n, board_line_min_y), (board_line_min_x + board_line_inc_x * n, board_line_max_y)) for n in range(board_lines)]
+    self._lines = x_lines + y_lines
+
+
+class Stone:
+
+  def getColor(self):
+    return self._color
+
+  def getRadius(self):
+    return self._radius
+
+  def getCenter(self):
+    return self._center
+
+  def moveBy(translation, bounds): # could use insets instead of bounds
+    x, y = self._center
+    tx, ty = translation
+    x, y = x + tx, y + ty
+    min_x, min_y, max_x, max_y = bounds
+    if x - self._radius < min_x:
+      x = min_x + self.radius
+    if y - self.radius < min_y:
+      y = min_y + self.radius
+    if x + self.radius > max_x:
+      x = max_x - self.radius
+    if y + self.radius > max_y:
+      y = max_y - self.radius
+
+  def __init__(self, color, radius, center):
+    self._color = color
+    self._radius = radius
+    self._center = center
+
+
+test()
 
