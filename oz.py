afa23bf8a232c8557db2e024170b52de2bb959cd
Jun Luo <luo2jun1@gmail.com>
2019-10-20 17:55:42 -0400

    Support oz env configuration through command line
    Closes #29

--- oz.py
+++ oz.py
@@ -325,6 +325,19 @@ def runOz(e, cycles=-1, timing=False):
     timeElapsed = end - start
     print("Cycles: ", cycles, "  Time elasped: ", timeElapsed, "  Time per cycle: ", timeElapsed / cycles)
 
+
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--lines', nargs='?', default=4, type=int)
+parser.add_argument('-s', '--stones', nargs='?', default=10, type=int)
+parser.add_argument('-a', '--agents', nargs='?', default=2, type=int)
+parser.add_argument('-x', '--width', nargs='?', default=1.0, type=float)
+parser.add_argument('-y', '--height', nargs='?', default=1.0, type=float)
+args = parser.parse_args()
+
+e = env.Environment(1.0, 1.0, args.lines, args.stones, args.agents) # five-agent tic-tac-toe
+
 # WITHOUT array3d
 # Before optimization --
 # Cycles:  1000   Time elasped:  2.808263063430786   Time per cycle:  0.0028082630634307863
@@ -337,7 +350,7 @@ def runOz(e, cycles=-1, timing=False):
 # Cycles:  1000   Time elasped:  1.3584859371185303   Time per cycle:  0.0013584859371185303
 # After optimization --
 # Cycles:  1000   Time elasped:  1.215559720993042   Time per cycle:  0.001215559720993042
-e = env.Environment(1.0, 1.0, 4, 10, 6) # five-agent tic-tac-toe
+#e = env.Environment(1.0, 1.0, 4, 10, 6) # five-agent tic-tac-toe
 
 # e = env.Environment()
 
