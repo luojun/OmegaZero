15ad54956848fe8818417918978b5204ddbde9de
Jun Luo <luo2jun1@gmail.com>
2019-11-09 05:19:02 -0500

    Add interface for running affordance0 experiment
    This satisfies #35

--- oz/__main__.py
+++ oz/__main__.py
@@ -13,15 +13,21 @@ def main():
                          default=config.DEFAULT_NUMBER_OF_AGENTS, type=int)
     _parser.add_argument('-hz', '--display_hz', nargs='?',
                          default=config.DEFAULT_DISPLAY_HZ, type=float)
+    _parser.add_argument('-e', '--experiment', nargs='?',
+                         default='', type=str)
     _args = _parser.parse_args()
 
     from .world.world import World
     from .world.settings import Settings
     from .runner.runner import Runner
     from .agent.agent import Agent
-    from .affordances.agent0 import Agent0
     _settings = Settings(config, _args.lines, _args.stones, _args.agents)
-    _world = World(_settings, [Agent, Agent0])
+
+    if _args.experiment == 'affordance0':
+        from .affordances.agent0 import Agent0
+        _world = World(_settings, [Agent, Agent0])
+    else:
+        _world = World(_settings)
     _runner = Runner(_world, resolution=config.DEFAULT_DISPLAY_RESOLUTION,
                              transparent_color_key=config.DEFAULT_TRANSPARENT_COLOR_KEY)
 
